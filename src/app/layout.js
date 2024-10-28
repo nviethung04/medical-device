ab116c812daeed50b58af725e9dd0e79551ac0bd
phamtienthanhcong <congphamtienthanh@gmail.com>
2024-10-28 10:03:15 +0700

    base

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,23 +1,17 @@
-import "@yeardle/assets/styles/main.css";
-import BaseLayout from "@yeardle/layouts/BaseLayout";
-import { GoogleAnalytics } from "@next/third-parties/google";
-import { GOOGLE_ANALYTICS_ID } from "@yeardle/constants/AppConfig";
-import { Providers } from "@yeardle/configs/providers";
-import { ColorModeScript } from "@chakra-ui/react";
-import theme from "@yeardle/configs/theme";
+"use client";
+import { baselightTheme } from "@/utils/theme/DefaultColors";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({children}) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
-      <head>
-        <link rel="icon" href="/images/LOGO/timedle-logo.png" />
-        {/* <GoogleAnalytics gaId={GOOGLE_ANALYTICS_ID} /> */}
-      </head>
-      <body suppressHydrationWarning={true}>
-        <Providers>
-          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-          <BaseLayout>{children}</BaseLayout>
-        </Providers>
+    <html lang="en">
+      <body>
+        <ThemeProvider theme={baselightTheme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
