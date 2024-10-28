8c9468c3d47cf1be9384412f790b2c7c94d424b5
phamtienthanhcong <congphamtienthanh@gmail.com>
2024-10-28 10:33:01 +0700

    remove style

--- src/utils/createEmotionCache.js
+++ src/utils/createEmotionCache.js
@@ -9,7 +9,7 @@ export default function createEmotionCache() {
   let insertionPoint;
 
   if (isBrowser) {
-    const emotionInsertionPoint = document.querySelector<HTMLMetaElement>(
+    const emotionInsertionPoint = document.querySelector(
       'meta[name="emotion-insertion-point"]',
     );
     insertionPoint = emotionInsertionPoint ?? undefined;
