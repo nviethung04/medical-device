301b337074c5e0fb1978783aee7072d5a01cc54d
phamtienthanhcong <congphamtienthanh@gmail.com>
2024-11-03 17:13:09 +0700

    "use client";

--- src/app/(DashboardLayout)/(page)/products/inventory/page.js
+++ src/app/(DashboardLayout)/(page)/products/inventory/page.js
@@ -1,3 +1,4 @@
+"use client";
 import PageContainer from "@/app/(DashboardLayout)/components/container/PageContainer";
 import DashboardCard from "@/app/(DashboardLayout)/components/shared/DashboardCard";
 import { Button } from "@mui/material";
